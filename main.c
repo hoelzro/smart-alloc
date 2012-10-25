5969cf6aa38bf133b13de1837ace8cb5d30621b8
Rob Hoelz <rob@hoelz.ro>
2012-10-25 18:28:57 +0200

    Add test for basic allocation

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include "smart-alloc.h"
 
 int
 main(int argc, char **argv)
 {
+    char *message = smart_alloc(sizeof("Hello, World!"));
+
+    strcpy(message, "Hello, World!");
+    printf("%s\n", message);
+    /*smart_free(message);*/
+
     return 0;
 }
